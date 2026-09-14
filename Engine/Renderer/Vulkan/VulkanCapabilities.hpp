1b210a60fef1eddf5783a4e770e0a393ea544967
imjaghi575-cmyk <imjaghi575@gmail.com>
2026-09-14 12:36:28 +0330

    fix: cap Vulkan API by physical device support

--- Engine/Renderer/Vulkan/VulkanCapabilities.hpp
+++ Engine/Renderer/Vulkan/VulkanCapabilities.hpp
@@ -7,6 +7,7 @@ namespace urbanduty::renderer::vulkan {
 VkApiVersion select_supported_api_version(
     VkApiVersion requested,
     VkApiVersion loader_version,
+    VkApiVersion physical_device_version,
     int android_api_level) noexcept;
 
 } // namespace urbanduty::renderer::vulkan
