3925130493211cf5f7106d1940bdd3a0e223cdd2
imjaghi575-cmyk <imjaghi575@gmail.com>
2026-09-14 12:07:59 +0330

    test: cover Vulkan API platform compatibility boundaries

--- Tests/VulkanCapabilitiesTests.cpp
+++ Tests/VulkanCapabilitiesTests.cpp
@@ -5,9 +5,10 @@
 int main() {
     using namespace urbanduty::renderer::vulkan;
 
-    assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_3, 33) == VK_API_VERSION_1_3);
-    assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_1, 33) == VK_API_VERSION_1_1);
+    assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_3, 0) == VK_API_VERSION_1_3);
+    assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_1, 0) == VK_API_VERSION_1_1);
     assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_3, 29) == VK_API_VERSION_1_1);
-    assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_0, 24) == VK_API_VERSION_1_0);
+    assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_3, 24) == VK_API_VERSION_1_0);
+    assert(select_supported_api_version(VK_API_VERSION_1_3, VK_API_VERSION_1_3, 33) == VK_API_VERSION_1_3);
     return 0;
 }
